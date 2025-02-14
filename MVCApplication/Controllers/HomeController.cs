c98daef2a6d310b810552f6fd40e6a6f5b53434a
Manish Sharma <manishms2407@gmail.com>
2025-02-14 11:18:29 +0530

    ASP.Net MVC Tutorial - Creation of Views, Models and use of ViewData and ViewBag to pass the data from COntroller to View

--- MVCApplication/Controllers/HomeController.cs
+++ MVCApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,34 +10,65 @@ namespace MVCApplication.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        //When adding the Controller and run it By default it will show "The view 'Index' or its master was not found or no view engine supports the searched locations"
+        //1. When adding the Controller and run it By default it will show "The view 'Index' or its master was not found or no view engine supports the searched locations"
+        //To resolve this issue we need to have the view related to it.
         //public ActionResult Index()
         //{
         //    return View();
         //}
 
-        //to check the controller is working or not we have changed the return type and run it.
+        //2. to check the controller is working or not we have changed the return type and run it.
         //public string Index()
         //{
         //    return "Hello MVC 5 Application";
         //}
 
-        //checking Controller with Id parameter in the actionResult
+        //3. checking Controller with Id parameter in the actionResult
         //public string Index(string id)
         //{
         //    return "The value of  Id = " + id;
         //}
 
-        //checking Controller by passing Id and name parameter in the actionResult
+        //4. checking Controller by passing Id and name parameter in the actionResult
         //public string Index(string id, string name)
         //{
         //    return "The value of  Id = " + id + " and Name = " + name;
         //}
 
-        //checking Controller by passing Id and name parameter and using Request.QueryString in the actionResult
-        public string Index(string id, string name)
+        //5. checking Controller by passing Id and name parameter and using Request.QueryString in the actionResult
+        //public string Index(string id, string name)
+        //{
+        //    return "Id = " + id + " ,Name = " + Request.QueryString["name"];
+        //}
+
+        //6. checking Controller by adding model and views in the separate folder
+        //public ActionResult Index(int id)
+        //{
+        //    EmployeeBusinessLayer employeeBL = new EmployeeBusinessLayer();
+        //    Employee employee = employeeBL.GetEmployeeDetails(id);
+        //    return View();
+        //}
+
+        //7. checking Controller by adding model and views in the separate folder and getting data using ViewData
+        //public ActionResult Index()
+        //{
+        //    EmployeeBusinessLayer employeeBL = new EmployeeBusinessLayer();
+        //    Employee employee = employeeBL.GetEmployeeDetails(102);
+
+        //    ViewData["Employee"] = employee;
+        //    ViewData["Header"] = "Getting Employee Details using ViewData";
+        //    return View();
+        //}
+
+        //8. checking Controller by adding model and views in the separate folder and getting data using ViewBag
+        public ActionResult Index()
         {
-            return "Id = " + id + " ,Name = " + Request.QueryString["name"];
+            EmployeeBusinessLayer employeeBL = new EmployeeBusinessLayer();
+            Employee employee = employeeBL.GetEmployeeDetails(102);
+
+            ViewBag.Employee = employee;
+            ViewBag.Header = "Getting Employee Details using ViewBag";
+            return View();
         }
     }
 }
---
+++ MVCApplication/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApplication.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public Employee GetEmployeeDetails(int EmployeeId)
+        {
+            //Here we hardcoded the data
+            //later we will discuss how to retrieve
+            //the data from a database
+            Employee employee = new Employee()
+            {
+                EmployeeId = EmployeeId,
+                Name = "Manish",
+                Gender = "Male",
+                City = "Gurugram",
+                Salary = 10000,
+                Address = "Sector 21"
+            };
+            return employee;
+        }
+    }
+}
