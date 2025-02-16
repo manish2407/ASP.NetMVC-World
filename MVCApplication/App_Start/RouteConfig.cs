5130c7b7629d7b1c8c7f756882ed21873ad746af
Manish Sharma <manishms2407@gmail.com>
2025-02-16 23:10:00 +0530

    ASP.Net MVC Tutorial - Creating Custom Route for the Application in RouteConfig and Creating Constraints for the Routes

--- MVCApplication/App_Start/RouteConfig.cs
+++ MVCApplication/App_Start/RouteConfig.cs
@@ -19,20 +19,25 @@ namespace MVCApplication
             //    defaults: new { controller = "Employee", action = "Details" },
             //    constraints: new { id = @"\d+" }
             //);
+            //routes.MapRoute(
+            // "Default", // Route name
+            // "{controller}/{action}/{id}", // Route Pattern
+            // new { controller = "Employee", action = "Details", id = UrlParameter.Optional }, // Default values for parameters
+            // new { controller = "^E.*", action = "^Details$" } //Restriction for controller and action
+            //);
             routes.MapRoute(
-             "Default", // Route name
-             "{controller}/{action}/{id}", // Route Pattern
-             new { controller = "Employee", action = "Details", id = UrlParameter.Optional }, // Default values for parameters
-             new { controller = "^E.*", action = "^Details$" } //Restriction for controller and action
+                name: "Employee",
+                url: " Employee/Details/{id}",
+                defaults: new { controller = "Employee", action = "Details" }
             );
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
 
-            
+
         }
     }
 }
