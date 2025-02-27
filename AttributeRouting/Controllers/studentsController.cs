c426af083e16d7c6dde7787c8e4b9304d5acd6fb
Manish Sharma <manishms2407@gmail.com>
2025-02-27 22:55:21 +0530

    ASP.Net MVC Tutorial - Concept of Route Constraint in Attribute Routing [parameter:constraint]

--- AttributeRouting/Controllers/studentsController.cs
+++ AttributeRouting/Controllers/studentsController.cs
@@ -10,7 +10,7 @@ namespace AttributeRouting.Controllers
     [RoutePrefix("students")]
     public class studentsController : Controller
     {
-        //Using Separate Routes for every Action Result
+        //1. Using Separate Routes for every Action Result
 
         //static List<Student> students = new List<Student>()
         //{
@@ -49,7 +49,48 @@ namespace AttributeRouting.Controllers
         //    return View();
         //}
 
-        //using Common Route Attribute at the Controller Level
+        //2. using Common Route Attribute at the Controller Level
+        //static List<Student> students = new List<Student>()
+        //{
+        //    new Student() { Id = 1, Name = "Pranaya" },
+        //    new Student() { Id = 2, Name = "Priyanka" },
+        //    new Student() { Id = 3, Name = "Anurag" },
+        //    new Student() { Id = 4, Name = "Sambit" }
+        //};
+        //[HttpGet]
+        //[Route]
+        ////This will be translated to /students 
+        //public ActionResult GetAllStudents()
+        //{
+        //    return View(students);
+        //}
+        //[HttpGet]
+        //[Route("{studentID}")]
+        ////This will be translated to /students/2
+        //public ActionResult GetStudentByID(int studentID)
+        //{
+        //    Student studentDetails = students.FirstOrDefault(s => s.Id == studentID);
+        //    return View(studentDetails);
+        //}
+        //[HttpGet]
+        //[Route("{studentID}/courses")]
+        ////This will be translated to /students/2/course
+        //public ActionResult GetStudentCourses(int studentID)
+        //{
+        //    List<string> CourseList = new List<string>();
+        //    if (studentID == 1)
+        //        CourseList = new List<string>() { "ASP.NET", "C#.NET", "SQL Server" };
+        //    else if (studentID == 2)
+        //        CourseList = new List<string>() { "ASP.NET MVC", "C#.NET", "ADO.NET" };
+        //    else if (studentID == 3)
+        //        CourseList = new List<string>() { "ASP.NET WEB API", "C#.NET", "Entity Framework" };
+        //    else
+        //        CourseList = new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
+        //    ViewBag.CourseList = CourseList;
+        //    return View();
+        //}
+
+        //4. Using Route COnstraints Concept in Attribute Routing
         static List<Student> students = new List<Student>()
         {
             new Student() { Id = 1, Name = "Pranaya" },
@@ -57,37 +98,21 @@ namespace AttributeRouting.Controllers
             new Student() { Id = 3, Name = "Anurag" },
             new Student() { Id = 4, Name = "Sambit" }
         };
+
         [HttpGet]
-        [Route]
-        //This will be translated to /students 
-        public ActionResult GetAllStudents()
-        {
-            return View(students);
-        }
-        [HttpGet]
-        [Route("{studentID}")]
-        //This will be translated to /students/2
-        public ActionResult GetStudentByID(int studentID)
+        [Route("{studentID:int}")] //Adding Route Constraint for int parameter 
+        public ActionResult GetStudentDetails(int studentID)
         {
             Student studentDetails = students.FirstOrDefault(s => s.Id == studentID);
             return View(studentDetails);
         }
+
         [HttpGet]
-        [Route("{studentID}/courses")]
-        //This will be translated to /students/2/course
-        public ActionResult GetStudentCourses(int studentID)
+        [Route("{studentName:alpha}")] //Adding Route Constraint for string parameter 
+        public ActionResult GetStudentDetails(string studentName)
         {
-            List<string> CourseList = new List<string>();
-            if (studentID == 1)
-                CourseList = new List<string>() { "ASP.NET", "C#.NET", "SQL Server" };
-            else if (studentID == 2)
-                CourseList = new List<string>() { "ASP.NET MVC", "C#.NET", "ADO.NET" };
-            else if (studentID == 3)
-                CourseList = new List<string>() { "ASP.NET WEB API", "C#.NET", "Entity Framework" };
-            else
-                CourseList = new List<string>() { "Bootstrap", "jQuery", "AngularJs" };
-            ViewBag.CourseList = CourseList;
-            return View();
+            Student studentDetails = students.FirstOrDefault(s => s.Name == studentName);
+            return View(studentDetails);
         }
     }
 }
