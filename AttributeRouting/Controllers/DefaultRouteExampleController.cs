e1e14d1574b553d436833b7e5f0ea826a0a95dfc
Manish Sharma <manishms2407@gmail.com>
2025-03-01 19:30:07 +0530

    ASP.Net MVC Tutorial - Concept of Default Route and Route Naming in the Attribute Routing

---
+++ AttributeRouting/Controllers/DefaultRouteExampleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AttributeRouting.Controllers
+{
+    [RoutePrefix("MyPage")]
+    [Route({"action = Index"})]
+    public class DefaultRouteExampleController : Controller
+    {
+        // URL: /MyPage/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Index Page";
+            return View();
+        }
+        // URL: /MyPage/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page";
+            return View();
+        }
+        // URL: /MyPage/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "About";
+            return View();
+        }
+    }
+}
