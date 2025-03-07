afa73196b6b76939d8dfd06e64d94e0f4b6e7c00
Manish Sharma <manishms2407@gmail.com>
2025-03-07 23:10:48 +0530

    ASP.Net MVC Tutorial - Creation of Controller and Views and modification Route.config file to render the content on the page

--- CRUDOperationInMVC/App_Start/RouteConfig.cs
+++ CRUDOperationInMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CRUDOperationInMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CRUDOperationInMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using CRUDOperationInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDOperationInMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            List<Employee> empList = dbContext.Employees.ToList();
+            return View(empList);
+        }
+    }
+}
