4c076a9fa3041cf86c204c2d74351f0089fb1d57
Manish Sharma <manishms2407@gmail.com>
2025-04-23 23:17:19 +0530

    ASP.Net MVC Tutorial - CRUD Operation in MVC using Entity Framework and ActionLink HtmlHelper

--- CRUDOperationInMVC/App_Start/RouteConfig.cs
+++ CRUDOperationInMVC/App_Start/RouteConfig.cs
@@ -9,14 +9,24 @@ namespace CRUDOperationInMVC
 {
     public class RouteConfig
     {
+        //public static void RegisterRoutes(RouteCollection routes)
+        //{
+        //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        //    routes.MapRoute(
+        //        name: "Default",
+        //        url: "{controller}/{action}/{id}",
+        //        defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
+        //    );
+        //}
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Department", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CRUDOperationInMVC/Controllers/NewEmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using CRUDOperationInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDOperationInMVC.Controllers
+{
+    public class NewEmployeeController : Controller
+    {
+        // GET: NewEmployee
+        public ActionResult Index(int departmentId)
+        {
+            NewEmployeeDbContext dbContext = new NewEmployeeDbContext();
+            List<NewEmployee> employees = dbContext.NewEmployees.Where(emp => emp.DepartmentId == departmentId).ToList();
+            return View(employees);
+        }
+        public ActionResult Details(int id)
+        {
+            NewEmployeeDbContext dbContext = new NewEmployeeDbContext();
+            NewEmployee employee = dbContext.NewEmployees.FirstOrDefault(x => x.EmployeeId == id);
+            return View(employee);
+        }
+    }
+}
